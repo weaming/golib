3ec95c695029dc36df6f0ba8b425f0c0fd9c0f9f
weaming <garden.yuen@gmail.com>
2018-05-02 06:31:26 +0800

    fix permission bug

--- fs/file.go
+++ fs/file.go
@@ -18,7 +18,7 @@ func CreateDirIfNotExist(dir string, force bool) error {
 	fi, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		// prepare dir
-		if err := os.MkdirAll(dir, 0644); err != nil {
+		if err := os.MkdirAll(dir, 0777); err != nil {
 			return err
 		}
 	} else {
@@ -38,7 +38,7 @@ func OpenFile(path string) (*os.File, error) {
 	path = ExpandUser(path)
 	dir := filepath.Dir(path)
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		err = os.MkdirAll(dir, 0644)
+		err = os.MkdirAll(dir, 0777)
 		if err != nil {
 			return nil, err
 		}
