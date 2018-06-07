9015fc475d85f0f9667b09366b2b21a044a8bfbd
weaming <garden.yuen@gmail.com>
2018-06-07 17:53:35 +0800

    update IndexOf test

--- abstract/indexof_test.go
+++ abstract/indexof_test.go
@@ -8,4 +8,7 @@ func TestIndexOf(t *testing.T) {
 	if IndexOf([]interface{}{"a", "b", "weaming", "d"}, "weaming") < 0 {
 		t.Fail()
 	}
+	if IndexOf([]interface{}{"a", "b", "weaming", "d"}, 3) >= 0 {
+		t.Fail()
+	}
 }
