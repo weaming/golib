255ff580d82b94ab2a0710acd001571b2764c50e
weaming <garden.yuen@gmail.com>
2018-08-06 00:08:20 +0800

    add is-valid-proxy

---
+++ bin/is-valid-proxy/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	proxylib "github.com/weaming/golib/proxy"
+)
+
+func main() {
+	url := flag.String("url", "https://www.baidu.com", "target url to test using proxy")
+	proxy := flag.String("proxy", "http://localhost:8123", "proxy url")
+	code := flag.Int("code", 200, "expected response status code")
+	timeout := flag.Int("timeout", 3, "timeout in seconds")
+
+	if proxylib.IsValidProxy(*url, *proxy, *code, *timeout) {
+		os.Exit(0)
+	} else {
+		os.Exit(1)
+	}
+}
